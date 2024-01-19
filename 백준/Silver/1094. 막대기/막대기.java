76b7e9f8e6e1e9af33afc3950dd3273fab755dde
tmdals1207 <126853163+tmdals1207@users.noreply.github.com>
2024-01-20 04:02:44 +0900

    [Silver V] Title: 막대기, Time: 216 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int X = scanner.nextInt();
+
+        int count = 0;
+
+        while (X > 0) {
+            if (X % 2 == 1) {
+
+                count++;
+            }
+            X /= 2;
+        }
+
+        System.out.println(count);
+
+        scanner.close();
+    }
+}
