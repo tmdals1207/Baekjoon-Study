3cc4dc309804c70a01b9ed491b30be630d855b14
홍승민 <ghdtmdalsdad@naver.com>
2024-02-21 20:29:02 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2044 ms, Memory: 185172 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[][] points = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            points[i][0] = scanner.nextInt();
+            points[i][1] = scanner.nextInt();
+        }
+
+        Arrays.sort(points, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return Integer.compare(o1[0], o2[0]);
+            }
+            return Integer.compare(o1[1], o2[1]);
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(points[i][0] + " " + points[i][1]);
+        }
+    }
+}
