e89d5e80795c46865b2edd66672e17bbd9f7222f
홍승민 <ghdtmdalsdad@naver.com>
2024-11-14 19:36:25 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 428 ms, Memory: 208692 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,30 @@
-def dfs(value, start, visited):
-  stack = [start]
-  while stack:
-    v = stack.pop()
-    if(not visited[v]):
-      visited[v] = True
-      
-      for neighbor in value[v]:
-        if not visited[neighbor]:
-            stack.append(neighbor)
+def dfs(i, con, visited):
+    stack = [i]
+    while stack:
+        v = stack.pop()
+        if not visited[v]:
+            visited[v] = True
+
+            for nei in con[v]:
+                if not visited[nei]:
+                    stack.append(nei)
 
 n, m = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
+
+con = [[] for _ in range(n + 1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    con[b].append(a)
+    con[a].append(b)
 
 cnt = 0
+
 visited = [False] * (n + 1)
+
 for i in range(1, n + 1):
     if not visited[i]:
-        dfs(graph, i, visited)
+        dfs(i, con, visited)
         cnt += 1
 
 print(cnt)
