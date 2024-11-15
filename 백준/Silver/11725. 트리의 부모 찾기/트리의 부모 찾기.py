400124bdab145f2c9474d1c7a7c2f06c951997c4
홍승민 <ghdtmdalsdad@naver.com>
2024-11-16 02:06:40 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 224 ms, Memory: 123624 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -7,18 +7,19 @@ def dfs(graph, start, parents):
                 parents[next] = current
                 stack.append(next)
 
-node = int(input())
 
-graph = [[] for _ in range(node + 1)]
+n = int(input())
 
-for _ in range(node - 1):
+graph = [[] for i in range(n + 1)]
+
+for _ in range(1, n):
     a, b = map(int, input().split())
-    graph[b].append(a)
     graph[a].append(b)
+    graph[b].append(a)
 
-parents = [0] * (node + 1)
+parents = [0] * (n + 1)
 
 dfs(graph, 1, parents)
 
-for i in range(2, node + 1):
+for i in range(2, n + 1):
     print(parents[i])
