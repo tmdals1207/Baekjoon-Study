8f7aa9014b971cfde3e0241764b9857294bf4fef
홍승민 <ghdtmdalsdad@naver.com>
2024-12-13 22:11:49 +0900

    [D3] Title: 피보나치 수 분배, Time: 139 ms, Memory: 48,300 KB -BaekjoonHub

---
+++ SWEA/D3/22039. 피보나치 수 분배/README.md
@@ -0,0 +1,15 @@
+# [D3] 피보나치 수 분배 - 22039 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZGSf4TaM08DFAXd) 
+
+### 성능 요약
+
+메모리: 48,300 KB, 시간: 139 ms, 코드길이: 717 Bytes
+
+### 제출 일자
+
+2024-12-13 22:11
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/22039. 피보나치 수 분배/피보나치 수 분배.py
@@ -0,0 +1,40 @@
+def fibonacci(n):
+    fib = [0] * (n + 1)
+    fib[1] = 1
+    if n > 1:
+        fib[2] = 1
+    for i in range(3, n + 1):
+        fib[i] = fib[i - 1] + fib[i - 2]
+    return fib
+
+
+def can_partition(N):
+    fib = fibonacci(N)
+    total_sum = sum(fib[1:N + 1])
+
+    if total_sum % 2 != 0:
+        return "impossible"
+
+    target = total_sum // 2
+    A = []
+    B = []
+
+    for i in range(N, 0, -1):
+        if target >= fib[i]:
+            A.append(i)
+            target -= fib[i]
+        else:
+            B.append(i)
+
+    result = ['B'] * N
+    for a in A:
+        result[a - 1] = 'A'
+
+    return ''.join(result)
+
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    result = can_partition(N)
+    print(result)
