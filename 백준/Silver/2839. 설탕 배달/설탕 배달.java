668456463d0be0fc3c347d6c42dff67a0bb9328b
홍승민 <ghdtmdalsdad@naver.com>
2025-02-12 14:14:18 +0900

    [Silver IV] Title: 설탕 배달, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,38 +1,46 @@
 import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int x = 0;
-        int y = 0;
-        
-        if(!isPossibleWith3And5(n)){
-                System.out.println("-1");
-            }
-        else{
-            System.out.println(calculator(n));
-        }
-    }
-    
-    public static int calculator(int num) {
-        int max = num / 5;
-        for(int i = max; i >= 0; i--){
-            int remainer = num - (i * 5);
-            if(remainer % 3 == 0){
-                return (i + (remainer / 3));
-            }
-        }
-        return -1;
-    }
-    
-    public static boolean isPossibleWith3And5(int num) {
-        for (int x = 0; 3 * x <= num; x++) {
-            for (int y = 0; 5 * y <= num; y++) {
-                if (3 * x + 5 * y == num) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-}
+
+public class Main {
+	
+	static int N; // N static 선언
+
+	private static int cal() {
+		int cnt = N/5; // 최대한 5kg로 들고가도록 설정
+
+		for (int i = cnt; i >= 0; i--) { // 5kg 짜리를 하나씩 빼가며 3kg로 대체
+			int remainder = N - (i*5); 
+			if(remainder % 3 == 0) { // 남아있던 값이 3kg로 나누어지면 종료
+				return (i + (remainder / 3));
+			}
+		}
+		
+		return -1;
+		
+	} // cal() 종료
+	
+	private static boolean isPossible() {
+		
+		for (int i = 0; i*3 <= N; i++) {
+			for (int j = 0; j*5 <= N; j++) {
+				if(i*3 + j*5 == N) {
+					return true;
+				}
+			}
+		} // 2중 for문 종료
+		return false;
+	} // isPossible() 종료
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in); // N 입력받기
+
+		N = scan.nextInt();
+		
+		int min = isPossible() ? cal(): -1; // 삼항연산자로 계산
+
+		System.out.println(min);
+		
+		scan.close();
+	} // main
+
+} // class
