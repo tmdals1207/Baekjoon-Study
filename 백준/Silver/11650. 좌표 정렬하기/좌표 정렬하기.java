fdce3e54e6aef8af8ecd0f9524a635fd75410069
홍승민 <ghdtmdalsdad@naver.com>
2025-02-13 10:43:19 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 688 ms, Memory: 50744 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,27 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int[][] points = new int[n][2];
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-        for (int i = 0; i < n; i++) {
-            points[i][0] = scanner.nextInt();
-            points[i][1] = scanner.nextInt();
-        }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int arr[][] = new int[N][2];
 
-        Arrays.sort(points, (o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return Integer.compare(o1[1], o2[1]);
-            }
-            return Integer.compare(o1[0], o2[0]);
-        });
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		// x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순으로.
+		Arrays.sort(arr, (o1, o2) -> 
+			o1[0] != o2[0] ? o1[0]-o2[0] : o1[1]-o2[1]);
+		
+		for (int j = 0; j < N; j++) {
+			bw.write(arr[j][0] + " " + arr[j][1] + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		
+	} // main
 
-        for (int i = 0; i < n; i++) {
-            System.out.println(points[i][0] + " " + points[i][1]);
-        }
-    }
-}
+} // class
