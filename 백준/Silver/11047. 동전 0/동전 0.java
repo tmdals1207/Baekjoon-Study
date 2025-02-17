becdf114a2a29092d78a803297986b85136be891
홍승민 <ghdtmdalsdad@naver.com>
2025-02-17 09:31:14 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,27 +1,34 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class Main {
 
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] coins = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] coins = new int[N];
+		int cnt = 0; // K원을 만드는데 필요한 동전 개수.
+		
+		// 오름차순으로 주어졌으니 정렬은 불필요.
+		for(int i = 0; i < N; i++) {
+			coins[i] = Integer.parseInt(br.readLine());
+		}
+		
+		for(int j = N-1; j>=0; j--) { // 가장 큰 동전의 가치부터 차례로 내려가며 계산.
+			if(K/coins[j]!=0) { // 1) 동전의 가치가 K원 보다 작으면.
+				cnt += K/coins[j]; // 2) cnt에 K원 나누기 해당 동전의 가치를 더함.
+				K %= coins[j]; // 3) K원은 해당 동전의 가치로 나누고 난 나머지로 변경. 
+			}
+		}
+		System.out.println(cnt);
+		br.close();
+	}
 
-        for (int i = 0; i < N; i++) {
-            coins[i] = scanner.nextInt();
-        }
-        
-        int count = 0;
-
-        for (int i = N - 1; i >= 0; i--) {
-            if (K >= coins[i]) {
-                count += K / coins[i];
-                K %= coins[i];
-            }
-        }
-
-        System.out.println(count);
-    }
 }
