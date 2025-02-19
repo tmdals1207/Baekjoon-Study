97379182f5603e31a0b6c48b75b1f3b063f01697
홍승민 <ghdtmdalsdad@naver.com>
2025-02-19 14:19:40 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,59 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Point {
-        int x, y;
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	static int N,M;
+	static int[][] board;
+	private static int[] dx = {0, 0, 1, -1};
+	private static int[] dy = {1, -1, 0, 0};
+	
+	private static int bfs() {
+		Queue<Square> queue = new LinkedList<>();
+		int [][] dist = new int[N][M];
+		
+		queue.add(new Square(0, 0));
+		dist[0][0] = 1;
+		
+		while(!queue.isEmpty()) {
+			Square node = queue.poll();
+			for(int i = 0; i < 4; i++) {
+				int nx = node.x + dx[i];
+				int ny = node.y + dy[i];
+				
+				if(nx>=0 && nx<N && ny>=0 && ny<M && board[nx][ny]==1 && dist[nx][ny] == 0) {
+					dist[nx][ny] = dist[node.x][node.y] +1;
+					queue.add(new Square(nx, ny));
+				}
+			}
+		}
+		return dist[N-1][M-1];
+	}
+	
+	public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        scanner.nextLine();
-
-        int[][] maze = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            String line = scanner.nextLine();
-            for (int j = 0; j < m; j++) {
-                maze[i][j] = line.charAt(j) - '0';
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		board = new int[N][M];
+		
+		for(int i = 0; i < N; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < M; j++) {
+                board[i][j] = line.charAt(j) - '0';
             }
-        }
-
-        int result = bfs(maze, n, m);
-        System.out.println(result);
-    }
-
-    private static int bfs(int[][] maze, int n, int m) {
-        Queue<Point> queue = new LinkedList<>();
-        int[][] dist = new int[n][m];
-        int[] dx = {0, 0, 1, -1};
-        int[] dy = {1, -1, 0, 0};
-
-        queue.add(new Point(0, 0));
-        dist[0][0] = 1;
-
-        while (!queue.isEmpty()) {
-            Point current = queue.poll();
-
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = current.x + dx[dir];
-                int ny = current.y + dy[dir];
-
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maze[nx][ny] == 1 && dist[nx][ny] == 0) {
-                    dist[nx][ny] = dist[current.x][current.y] + 1;
-                    queue.add(new Point(nx, ny));
-                }
-            }
-        }
-
-        return dist[n - 1][m - 1];
-    }
+		}
+		System.out.println(bfs());
+	}
+	
+	static class Square{
+		int x, y;
+		
+		public Square (int x, int y) {
+			this.x = x;
+			this.y =y;
+		}
+	}
 }
