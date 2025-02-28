7a5dc4e313bb176b5353abeae9f8f851bf6d2a8e
홍승민 <ghdtmdalsdad@naver.com>
2025-02-28 10:37:38 +0900

    [Gold IV] Title: 연구소, Time: 304 ms, Memory: 135008 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -7,72 +7,68 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
-	// 풀이법.
-	// 1. dfs로 벽 3개 세우기 함.
-	// 2. 벽 3개 세우면 dfs 돌리고 안전 영역 크기 저장한 다음에 백트래킹함
-	// 3. 해결ㅋ
-	
 	static int N, M, max;
 	static final int[] dx = {1, -1, 0, 0};
 	static final int[] dy = {0, 0, 1, -1};
 	
 	
-	static void infectionBfs(int[][] tmap) {
-		int[][] map = new int[N][M];
-	    for (int i = 0; i < N; i++) {
-	        System.arraycopy(tmap[i], 0, map[i], 0, M);
-	    }
-		Queue<Point> queue = new LinkedList<Point>();
+	static void wallDfs(int i, int j, int cnt, int[][] map) {
 		
-		for (int i = 0; i < N; i++) {
-	        for (int j = 0; j < M; j++) {
-	            if (map[i][j] == 2) {
-	                queue.add(new Point(i, j));
-	            }
-	        }
-	    }
-		
-		while(!queue.isEmpty()) {
-			Point current = queue.poll();
-			for(int i = 0; i < 4; i++) {
-				int nx = current.x+dx[i];
-				int ny = current.y+dy[i];
-				
-				if(nx>=0 && nx<N && ny>=0 && ny<M && map[nx][ny]==0) {
-					map[nx][ny] = 2;
-					queue.add(new Point(nx, ny));
-				}
-			}
-		}
-		
-		int safeZone = 0;
-	    for (int i = 0; i < N; i++) {
-	        for (int j = 0; j < M; j++) {
-	            if (map[i][j] == 0) {
-	                safeZone++;
-	            }
-	        }
-	    }
-	    max = Math.max(max, safeZone);
-		
-	}
-	
-	static void wallDfs (int x, int y, int cnt, int[][] map) {
-		if(cnt == 3) {
+		if (cnt == 3) {
 			infectionBfs(map);
 			return;
 		}
 		
-		for (int i = x; i < N; i++) {
-			for (int j = (i == x ? y : 0); j < M; j++) {
-				if(map[i][j]==0) {
-					map[i][j] = 1;
-					wallDfs(i, j+1, cnt+1, map);
-					map[i][j]=0;
+		for (int x = i; x < N; x++) {
+			for (int y = (x == i ? j : 0); y < M; y++) {
+				if(map[x][y]==0) {
+					map[x][y] = 1;
+					wallDfs(x, y+1, cnt+1, map);
+					map[x][y]=0;
 				}
 			}
 		}
 		
+	}
+
+	private static void infectionBfs(int[][] map) {
+		Queue<Point> queue = new LinkedList<Point>();
+		int[][] newMap = new int[N][M];
+		
+		for (int i = 0; i < N; i++) {
+	        System.arraycopy(map[i], 0, newMap[i], 0, M);
+	    }
+		
+		for (int a = 0; a < N; a++) {
+			for (int b = 0; b <M; b++) {
+				if(newMap[a][b]==2) {
+					queue.add(new Point(a, b));
+				}
+			}
+		}
+		
+		while(!queue.isEmpty()) {
+			Point cur = queue.poll();
+			
+			for(int i = 0; i < 4; i++) {
+				int nx = cur.x+dx[i];
+				int ny = cur.y+dy[i];
+				
+				if(nx>=0 && nx<N && ny>=0 && ny<M && newMap[nx][ny]==0) {
+					newMap[nx][ny] = 2;
+					queue.add(new Point(nx, ny));
+				}
+			}
+		}
+		int safeZone = 0;
+		for (int k = 0; k < N; k++) {
+			for (int j = 0; j < M; j++) {
+				if(newMap[k][j]==0) {
+					safeZone++;
+				}
+			}
+		}
+		max = Math.max(max, safeZone);
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -93,16 +89,17 @@ public class Main {
 		}
 		
 		wallDfs(0, 0, 0, map);
+		
 		System.out.println(max);
+
 	}
-
-}
-
-class Point {
+public static class Point {
 	int x, y;
 	
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
+}
+    
 }
