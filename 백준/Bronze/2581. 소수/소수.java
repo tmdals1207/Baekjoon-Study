3860d99e9d2eb1d5cc98415a79d39065acdef9c4
홍승민 <ghdtmdalsdad@naver.com>
2025-03-02 19:40:37 +0900

    [Bronze II] Title: 소수, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int min = Integer.MAX_VALUE, sum = 0;
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                sum += i;
+                if (min == Integer.MAX_VALUE) min = i;
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+
+    static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
