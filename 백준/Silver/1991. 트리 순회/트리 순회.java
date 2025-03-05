0f427792be4b9ffe45c0b68ea4b298a1673ebeec
홍승민 <ghdtmdalsdad@naver.com>
2025-03-05 23:15:56 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,47 +1,52 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	static int N;
-	static Node nodes[];
+	static Node[] nodes;
+	static BufferedWriter bw;
 	
-	static void preorder(Node node) {
-		System.out.print(node.value);
+	
+	static void preorder(Node node) throws IOException {
+		bw.write(node.data);
 		if(node.left!=null) preorder(node.left);
 		if(node.right!=null) preorder(node.right);
 	}
 	
-	static void inorder(Node node) {
+	static void inorder(Node node) throws IOException {
 		if(node.left!=null) inorder(node.left);
-		System.out.print(node.value);
-		if(node.right!=null) inorder(node.right);
+		bw.write(node.data);
+		if(node.right!=null) inorder(node.right);	
 	}
-
-	static void postorder(Node node) {
+	
+	static void postorder(Node node) throws IOException {
 		if(node.left!=null) postorder(node.left);
 		if(node.right!=null) postorder(node.right);
-		System.out.print(node.value);
-	}
-	
+		bw.write(node.data);
+	}	
 	
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(st.nextToken());
 		nodes = new Node[N];
 		
 		for (int i = 0; i < N; i++) {
 			nodes[i] = new Node((char) ('A'+i));
 		}
 		
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-
+		for(int i = 0; i < N; i ++) {
+			st = new StringTokenizer(br.readLine());
+			
 			char node = st.nextToken().charAt(0);
 			char left = st.nextToken().charAt(0);
 			char right = st.nextToken().charAt(0);
@@ -53,23 +58,25 @@ public class Main {
 		}
 		
 		preorder(nodes[0]);
-		System.out.println();
+		bw.write("\n");
+		bw.flush();
 		inorder(nodes[0]);
-		System.out.println();
+		bw.write("\n");
+		bw.flush();
 		postorder(nodes[0]);
-		br.close();
-		
+		bw.close();
 	}
 
 }
 
-class Node {
-    char value;
-    Node left, right;
 
-    Node(char value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+class Node {
+	char data;
+	Node left, right;
+	
+	public Node (char data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
 }
