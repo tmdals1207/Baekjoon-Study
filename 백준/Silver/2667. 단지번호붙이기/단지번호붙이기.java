c629cb019b6e4e2ba4f50960e83ed079f11f5278
홍승민 <ghdtmdalsdad@naver.com>
2025-04-01 18:43:36 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,56 +1,82 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
 
-public class Main {	
-	static int[] di={-1,0,1,0};
-	static int[] dj={0,1,0,-1};
-	static int N,cnt;
-	static int[][] map;
-	static boolean[][] v;
-	
-	public static void main(String[] args) throws Exception{
-		Scanner sc=new Scanner(System.in);
-		N=sc.nextInt();
-		map=new int[N][N];
-		v=new boolean[N][N];
-		for(int i=0; i<N; i++){
-			String s=sc.next();
-			for(int j=0; j<N; j++){
-				map[i][j]=s.charAt(j)-'0';
-			}
-		}
-		
-		PriorityQueue<Integer> pq=new PriorityQueue<>();
-		for(int i=0; i<N; i++){
-			for(int j=0; j<N; j++){
-				if(map[i][j]==1 && !v[i][j]){
-					pq.offer(bfs(i,j));
-				}
-			}
-		}
-		
-		System.out.println(pq.size());
-		while(!pq.isEmpty()) System.out.println(pq.poll());
-		sc.close();
-	}
-	static int bfs(int i,int j){
-		Queue<int[]> q=new ArrayDeque<int[]>();
-		int cnt=1;
-		v[i][j]=true;
-		q.offer(new int[]{i,j});
-		while(!q.isEmpty()){
-			int[] ij=q.poll();
-			i=ij[0];
-			j=ij[1];
-			for(int d=0; d<4; d++){
-				int ni=i+di[d];
-				int nj=j+dj[d];
-				if(0<=ni&&ni<N && 0<=nj&&nj<N && !v[ni][nj] && map[ni][nj]==1){
-					cnt++;
-					v[ni][nj]=true;
-					q.offer(new int[]{ni,nj});
-				} // if
-			} // for
-		} // while
-		return cnt;
-	}
+public class Main {
+
+    static int N;
+    static int[][] board;
+    static boolean[][] visited;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+
+    static int dfs(int i, int j) {
+        int count = 1;
+        Stack<Point> stack = new Stack<>();
+        stack.push(new Point(i, j));
+        visited[i][j] = true;
+
+        while (!stack.isEmpty()) {
+            Point cur = stack.pop();
+
+            for (int k = 0; k < 4; k++) {
+                int nx = cur.x + dx[k];
+                int ny = cur.y + dy[k];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny] && board[nx][ny] == 1) {
+                    stack.push(new Point(nx, ny));
+                    visited[nx][ny] = true;
+                    count ++;
+
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        board = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < N; j++) {
+                board[i][j] = input.charAt(j) - '0';
+            }
+        }
+
+        ArrayList<Integer> groupList = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 1 && !visited[i][j]) {
+                    groupList.add(dfs(i, j));
+                }
+            }
+        }
+
+        System.out.println(groupList.size());
+
+        Collections.sort(groupList);
+
+        for (Integer integer : groupList) {
+            System.out.println(integer);
+        }
+    }
+
+    static class Point {
+        int x;
+        int y;
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
