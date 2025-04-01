dea2f47c06cdfed06672e59304ca1cbfe1e52383
홍승민 <ghdtmdalsdad@naver.com>
2025-04-01 13:27:46 +0900

    [Silver II] Title: DFS와 BFS, Time: 228 ms, Memory: 20664 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,84 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int N, M, V;
-	static int[][] graph;
-	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
-	
-	private static void dfs(int v) {
-		Stack<Integer> stack = new Stack<>();
-		stack.push(v);
-		
-		while(!stack.isEmpty()) {
-			int node = stack.pop();
-			
-			if(!visited[node]) {
-				visited[node] = true;
-				sb.append(node + " ");
-				for(int i = graph.length-1; i > 0; i--) {
-					if(graph[node][i] == 1 && !visited[i]) {
-						stack.push(i);
-					}  // if
-				}  // for
-			}  // if
-		} // while
-	} // dfs 
-	
-	private static void bfs(int v) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(v);
-		visited[v] = true;
-		while(!queue.isEmpty()) {
-			int node = queue.poll();
-			sb.append(node + " ");
-			for(int i = 1; i <= graph.length-1; i++) {
-				if(graph[node][i] == 1 && !visited[i]) {
-					queue.offer(i);
-					visited[i] = true;
-				} // if
-			} // for
-		} // while
-	} // bfs
-	
-	public static void main(String[] args) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		V = Integer.parseInt(st.nextToken());
-		
-		graph = new int[N+1][N+1];
-		visited = new boolean[N+1];
-		
-		for(int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			graph[a][b] = 1;
-			graph[b][a] = 1;
-		}
+    static int N, M, V;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static BufferedWriter bw;
 
-		dfs(V);
-		
-		sb.append("\n");
-		
-		visited = new boolean[N+1];
-		
-		bfs(V);
+    static void dfs(int v) throws IOException {
+        visited[v] = true;
+        bw.write(v + " ");
 
-		System.out.println(sb);
-	}
+        for (int w : graph.get(v)) {
+            if (!visited[w]) {
+                dfs(w);
+            }
+        }
+    }
 
+    static void bfs(int v) throws IOException {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(v);
+        visited[v] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            bw.write(cur + " ");
+
+            for (int w : graph.get(cur)) {
+                if (!visited[w]) {
+                    visited[w] = true;
+                    queue.add(w);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i <= N; i++) graph.add(new ArrayList<>());
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        dfs(V);
+        bw.newLine();
+
+        Arrays.fill(visited, false);
+        bfs(V);
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
+    }
 }
