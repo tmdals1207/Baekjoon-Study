9d25e99b32c2c2225fc22209c33bf1406af48eba
HongSeungMin <ghdtmdalsdad@naver.com>
2025-04-19 23:16:47 +0900

    [Silver I] Title: 절댓값 힙, Time: 280 ms, Memory: 25572 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,38 +1,42 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                int absO1 = Math.abs(o1);
-                int absO2 = Math.abs(o2);
-                if (absO1 == absO2) {
+                if (Math.abs(o1) == Math.abs(o2)) {
                     return o1 - o2;
                 }
-                return absO1 - absO2;
+                return Math.abs(o1) - Math.abs(o2);
             }
         });
 
-        for (int i = 0; i < n; i++) {
-            int x = Integer.parseInt(br.readLine());
-            if (x == 0) {
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
                 if (pq.isEmpty()) {
                     sb.append("0\n");
-                } else {
+                }
+                else {
                     sb.append(pq.poll()).append("\n");
                 }
-            } else {
-                pq.offer(x);
+            }
+            else {
+                pq.offer(input);
             }
         }
-
         System.out.println(sb);
     }
 }
