2932b41ecbc48c9e28415b260b41a1d913198ec1
HongSeungMin <ghdtmdalsdad@naver.com>
2025-04-22 22:39:01 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -12,22 +12,25 @@ public class Main {
 	static BufferedWriter bw;
 	
 	
+    // 전위 순회 : Root → Left → Right
 	static void preorder(Node node) throws IOException {
-		bw.write(node.data);
-		if(node.left!=null) preorder(node.left);
-		if(node.right!=null) preorder(node.right);
+		bw.write(node.data); // 현재 노드 출력
+        if (node.left != null) preorder(node.left); // 왼쪽 자식 방문
+        if (node.right != null) preorder(node.right); // 오른쪽 자식 방문
 	}
 	
+    // 중위 순회 : Left → Root → Right
 	static void inorder(Node node) throws IOException {
-		if(node.left!=null) inorder(node.left);
-		bw.write(node.data);
-		if(node.right!=null) inorder(node.right);	
+		if (node.left != null) inorder(node.left); // 왼쪽 자식 방문
+        bw.write(node.data); // 현재 노드 출력
+        if (node.right != null) inorder(node.right); // 오른쪽 자식 방문	
 	}
 	
+    // 후위 순회 : Left → Right → Root
 	static void postorder(Node node) throws IOException {
-		if(node.left!=null) postorder(node.left);
-		if(node.right!=null) postorder(node.right);
-		bw.write(node.data);
+		if (node.left != null) postorder(node.left); // 왼쪽 자식 방문
+        if (node.right != null) postorder(node.right); // 오른쪽 자식 방문
+        bw.write(node.data); // 현재 노드 출력
 	}	
 	
 
@@ -44,25 +47,30 @@ public class Main {
 			nodes[i] = new Node((char) ('A'+i));
 		}
 		
-		for(int i = 0; i < N; i ++) {
-			st = new StringTokenizer(br.readLine());
-			
-			char node = st.nextToken().charAt(0);
-			char left = st.nextToken().charAt(0);
-			char right = st.nextToken().charAt(0);
-			
-			Node cur =  nodes[node-'A'];
-			cur.left = (left!='.') ? nodes[left-'A']:null;
-			cur.right = (right!='.') ? nodes[right-'A']:null;
-			
-		}
+		for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            char node = st.nextToken().charAt(0); // 부모 노드
+            char left = st.nextToken().charAt(0); // 왼쪽 자식 노드
+            char right = st.nextToken().charAt(0); // 오른쪽 자식 노드
+
+            // 현재 노드 찾기
+            Node cur = nodes[node - 'A'];
+
+            // '.'이 아니면 해당 노드를 왼쪽 자식으로 연결
+            cur.left = (left != '.') ? nodes[left - 'A'] : null;
+            // '.'이 아니면 해당 노드를 오른쪽 자식으로 연결
+            cur.right = (right != '.') ? nodes[right - 'A'] : null;
+        }
 		
 		preorder(nodes[0]);
 		bw.write("\n");
 		bw.flush();
+        
 		inorder(nodes[0]);
 		bw.write("\n");
 		bw.flush();
+        
 		postorder(nodes[0]);
 		bw.close();
 	}
